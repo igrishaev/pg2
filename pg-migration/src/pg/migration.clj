941c854d733dc8452b1df20d9289bc95d8f8a04c
Ivan Grishaev <ivan@grishaev.me>
2024-02-29 11:45:00 +0300

    updated

--- pg-migration/src/pg/migration.clj
+++ pg-migration/src/pg/migration.clj
@@ -2,10 +2,10 @@
   (:gen-class)
   (:import
 
-java.net.URI
-java.nio.file.FileSystem
-java.nio.file.FileSystems
-java.nio.file.Files
+   java.net.URI
+   java.nio.file.FileSystem
+   java.nio.file.FileSystems
+   java.nio.file.Files
 
    java.io.File
    java.lang.AutoCloseable)
@@ -236,9 +236,7 @@ java.nio.file.Files
 
 (defn group-parsed-files [parsed-files]
   (reduce
-   (fn [acc {:as foo
-             :keys [id slug direction file]}]
-
+   (fn [acc {:keys [id slug direction file]}]
      (let [node
            (cond-> {:id id
                     :slug slug}
@@ -250,13 +248,33 @@ java.nio.file.Files
              (assoc :file-next file))]
 
        (update acc id merge node)))
-
    (sorted-map)
    parsed-files))
 
 
 (defn is-directory? [^File file]
-  (not (.isDirectory file)))
+  (.isDirectory file))
+
+
+(defn validate-parsed-files [parsed-files]
+  (let [f
+        (juxt :id :direction)
+
+        result
+        (group-by f parsed-files)]
+
+    (doseq [[[id direction] items] result]
+      (when (-> items count (> 1))
+        (let [files
+              (for [item items]
+                (-> item ^File (:file) .getName))]
+          (pg/error! "Migration %s has %s %s files: %s"
+                     id
+                     (count items)
+                     (name direction)
+                     (str/join ", " files))))))
+
+  parsed-files)
 
 
 (defn read-file-migrations [^String path]
@@ -264,12 +282,17 @@ java.nio.file.Files
        (io/resource)
        (io/file)
        (file-seq)
-       (filter is-directory?)
+       (remove is-directory?)
        (map parse-file)
        (filter some?)
+       (validate-parsed-files)
        (group-parsed-files)
-       (vals)
-       (mapv map->Migration)))
+
+       ;;
+       ;; (vals)
+       ;; (mapv map->Migration)
+
+       ))
 
 
 (defn get-applied-migration-ids [conn]
@@ -280,7 +303,8 @@ java.nio.file.Files
     (->> query
          (pgh/query conn)
          (map :id)
-         (set))))
+         (set)
+         (not-empty))))
 
 
 (def DEFAULTS
@@ -347,6 +371,91 @@ java.nio.file.Files
       println))
 
 
+(defn check-migration-conflicts [migrations]
+  (doseq [i (range 0 (-> migrations count dec))]
+    (let [migration1 (get migrations i)
+          migration2 (get migrations (inc i))]
+      (when (and (-> migration1 :applied? not)
+                 (-> migration2 :applied?))
+        (pg/error! "Migration conflict: migration %s was applied before %s"
+                   (:id migration2)
+                   (:id migration1))))))
+
+
+(defn INIT [config]
+
+  (let [config+
+        (merge DEFAULTS config)
+
+        {:keys [migrations-table
+                migrations-path]}
+        config+
+
+        conn
+        (pg/connect config+)
+
+        _
+        (ensure-table conn migrations-table)
+
+        migrations
+        (read-file-migrations migrations-path)
+
+        applied-ids-set
+        (get-applied-migration-ids conn)
+
+        ;; id-max
+        ;; (when (seq applied-ids-set)
+        ;;   (apply max applied-ids-set))
+
+        migrations+
+        (update-vals migrations
+                     (fn [{:as migration :keys [id]}]
+                       (let [flag (contains? applied-ids-set id)]
+                         (assoc migration :applied? flag))))
+
+]
+
+    #_
+    (new Registry
+         conn
+         index
+         migrations+
+         migrations-table)))
+
+
+#_
+(defn --apply-next [conn migations]
+
+  (let [migration
+        (get migrations (inc index))]
+
+    (cond
+
+      (nil? migration)
+      this
+
+      (-applied? migration)
+      (-> this
+          (update :index inc))
+
+      :else
+      (let [{:keys [id
+                    slug
+                    file-next]}
+
+            migration
+
+            sql
+            (slurp file-next)]
+
+        (pg/query conn sql)
+        (pgh/insert-one conn migrations-table {:id id :slug slug})
+
+        (-> this
+            (update :index inc)
+            (update-in [:migrations index] -applied? true))))))
+
+
 #_
 (comment
 
@@ -356,6 +465,15 @@ java.nio.file.Files
      :user "test"
      :password "test"
      :database "test"})
+
+  (def -conn
+    (pg/connect -config))
+
+  (ensure-table -conn :migrations)
+
+  (read-file-migrations "migrations")
+
+  (get-applied-migration-ids -conn)
 
   (def -reg
     (init-registry -config))
