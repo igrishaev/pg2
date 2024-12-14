1a10958b6e946f23078033afe2589b0a92f04554
Ivan Grishaev <ivan@grishaev.me>
2024-12-14 19:31:12 +0300

    debug

--- pg-core/src/java/org/pg/util/Debug.java
+++ pg-core/src/java/org/pg/util/Debug.java
@@ -2,7 +2,7 @@ package org.pg.util;
 
 public final class Debug {
 
-    public static final boolean isON = true; // System.getenv("PG_DEBUG") != null;
+    public static final boolean isON = System.getenv("PG_DEBUG") != null;
 
     public static void debug(final String template, final Object... args) {
         System.out.printf((template) + "%n", args);
