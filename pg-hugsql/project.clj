9880bbd1e41e3107eb3dd9f36fdbacf4f17977ca
Ivan Grishaev <ivan@grishaev.me>
2024-12-22 13:06:57 +0300

    Version 0.1.26

--- pg-hugsql/project.clj
+++ pg-hugsql/project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.igrishaev/pg2-hugsql "0.1.26-SNAPSHOT"
+(defproject com.github.igrishaev/pg2-hugsql "0.1.26"
 
   :description
   "HugSQL wrapper for PG2"
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject _ "0.1.26-SNAPSHOT"
+(defproject _ "0.1.26"
 
   :url
   "https://github.com/igrishaev/pg2"
