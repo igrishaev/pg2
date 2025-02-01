5808b36d11fd66762185c62aeaf957e537c88a8e
Joshua Davey <josh@joshuadavey.com>
2025-02-01 18:20:59 -0500

    Move socket option setting into class

--- pg-core/src/java/org/pg/util/SocketTool.java
+++ pg-core/src/java/org/pg/util/SocketTool.java
@@ -12,19 +12,6 @@ import java.nio.channels.SocketChannel;
 
 public class SocketTool {
 
-    public static void setSocketOptions(Socket socket, Config config) {
-        try {
-            socket.setTcpNoDelay(config.SOTCPnoDelay());
-            socket.setSoTimeout(config.SOTimeout());
-            socket.setKeepAlive(config.SOKeepAlive());
-            socket.setReceiveBufferSize(config.SOReceiveBufSize());
-            socket.setSendBufferSize(config.SOSendBufSize());
-        }
-        catch (IOException e) {
-            throw new PGError(e, "couldn't set socket options");
-        }
-    }
-
     public static SocketChannel open(final SocketAddress address) {
         try {
             return SocketChannel.open(address);
