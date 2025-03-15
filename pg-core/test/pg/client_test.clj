2cc05a3c3e9f6584d6b141b01a55776768cc58b3
Ivan Grishaev <ivan@grishaev.me>
2025-03-15 16:45:49 +0300

    fix poll test

--- pg-core/test/pg/client_test.clj
+++ pg-core/test/pg/client_test.clj
@@ -978,6 +978,8 @@ from
           (let [amount (pg/poll-notifications conn2)]
             (swap! counter! + amount)))))
 
+    (Thread/sleep 100) ;; let executor finish
+
     (is (= #{{:channel channel
               :msg :NotificationResponse
               :self? false
