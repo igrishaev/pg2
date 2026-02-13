de86ca7289722d77819150e4c0330df97ff91701
Ivan Grishaev <ivan@grishaev.me>
2026-02-13 20:31:44 +0300

    Version 0.1.45-SNAPSHOT

--- pg-honey/project.clj
+++ pg-honey/project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.igrishaev/pg2-honey "0.1.44"
+(defproject com.github.igrishaev/pg2-honey "0.1.45-SNAPSHOT"
 
   :description
   "HoneySQL wrapper for PG2"
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject _ "0.1.44"
+(defproject _ "0.1.45-SNAPSHOT"
 
   :url
   "https://github.com/igrishaev/pg2"
