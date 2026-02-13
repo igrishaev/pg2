b02f42a536e441443e50f671f0dcdf6d03697bd4
Ivan Grishaev <ivan@grishaev.me>
2026-02-13 20:31:44 +0300

    Version 0.1.45-SNAPSHOT

--- pg-component/project.clj
+++ pg-component/project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.igrishaev/pg2-component "0.1.44"
+(defproject com.github.igrishaev/pg2-component "0.1.45-SNAPSHOT"
 
   :description
   "Component wrapper for PG2"
--- pg-hugsql/project.clj
+++ pg-hugsql/project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.igrishaev/pg2-hugsql "0.1.44"
+(defproject com.github.igrishaev/pg2-hugsql "0.1.45-SNAPSHOT"
 
   :description
   "HugSQL wrapper for PG2"
